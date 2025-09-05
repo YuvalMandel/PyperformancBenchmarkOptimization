c7de45c008f4ef9ea634f518e0293b8b97283e55
Yuval Mandel <tubal.mandel.212@gmail.com>
2025-09-05 16:59:00 +0300

    fixed script issue with run problem

--- script_crypto_pyaes.sh
+++ script_crypto_pyaes.sh
@@ -5,6 +5,8 @@ python3-dbg -m pip install --user py-spy
 export PATH="/root/.local/bin:$PATH"
 source /root/.bashrc
 python3-dbg -m pip install --user pycryptodome
+python3-dbg -m pip install --user numpy
+python3-dbg -m pip install --user numba
 # Benchmark execution using pyperformance and validating correctness
 original_runtime=$(python3-dbg pyaes/original/run_benchmark.py \
   | awk '/Mean/ {
@@ -19,7 +21,15 @@ pycryptodome_runtime=$(python3-dbg pyaes/pycryptodome/pycryptodome_runbenchmark.
       else if ($7 == "us") {print $6}
   }')
 echo "Pycryptodome runtime: ${pycryptodome_runtime} us"
+python3-dbg pyaes/numpy_numba/numpy_numba_validate.py
+numpy_numba_runtime=$(python3-dbg pyaes/numpy_numba/numpy_numba_runbenchmark.py \
+  | awk '/Mean/ {
+      if ($7 == "ms") {print $6 * 1000}
+      else if ($7 == "us") {print $6}
+  }')
+echo "numpy_numba runtime: ${numpy_numba_runtime} us"
 
 # Flame graph and performance data generation.
 py-spy record -o pyaes_profile.speedscope --format speedscope --rate 300 python3-dbg pyaes/original/pyaes_flamegraph_profile.py
 py-spy record -o pycryptodome_profile.speedscope --format speedscope --rate 10000 python3-dbg pyaes/pycryptodome/pycryptodome_flamegraph_profile.py
+py-spy record -o numpy_numba_profile.speedscope --format speedscope --rate 300 python3-dbg pyaes/numpy_numba/numpy_numba_flamegraph_profile.py
